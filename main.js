f7c493204bb5281552e8e9e8b0ce572d2287e5f0
stefanom13 <stefanomancuso13@gmail.com>
2022-02-08 16:20:23 +0100

    inserimento active

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 // dichiare variabili 
+let hamburgerOpen = document.querySelectorAll(".fas.fa-bars");
+let hamburgerMenu = document.querySelector(".hamburger-menu");
+let closeHamburger = document.querySelector(".fas.fa-times");
 
 // stamparli in console 
+console.log(hamburgerOpen, hamburgerMenu, closeHamburger);
 
 // aggiunta click con le sue funzioni 
+hamburgerOpen.addEventListener("click", function(){
+    
+    hamburgerMenuLink.classList.add("active");
+
+})
+
+
+
+
